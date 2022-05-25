804d0a2edb71ce0881461e1908eef77062cc6aad
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-05-25 23:09:56 +0600

    delete operation for profile

--- index.js
+++ index.js
@@ -78,6 +78,12 @@ async function run() {
             const result = await bookedCollection.deleteOne(query);
             res.send(result);
         });
+        app.delete('/updateProfile/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookedCollection.deleteOne(query);
+            res.send(result);
+        });
 
     } finally {
 
