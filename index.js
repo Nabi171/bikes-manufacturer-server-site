b479bc25513f0c2d5bea4e15b4949bb2c30f20c7
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-05-27 00:27:01 +0600

    userCollection added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 4000;
@@ -11,12 +12,31 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ynfqx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UNauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+
+    });
+}
+
+
 async function run() {
     await client.connect();
     const toolsCollection = client.db("bikeTools").collection("tools");
     const bookedCollection = client.db('bikeTools').collection('bookedTools');
     const reviewCollection = client.db('bikeTools').collection('reviews');
     const profileUpdateCollection = client.db('bikeTools').collection('updateProfile');
+    const userCollection = client.db('bikeTools').collection('users');
     console.log('connected to db jkfdlasljk');
     try {
         app.get('/tools', async (req, res) => {
@@ -55,6 +75,26 @@ async function run() {
             console.log(results);
             res.send(results);
         });
+        app.get('/user', async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
 
         app.post('/tools', async (req, res) => {
             const newTool = req.body;
@@ -75,6 +115,19 @@ async function run() {
             const update = req.body;
             const result = await profileUpdateCollection.insertOne(update);
             res.send(result);
+        });
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
         });
         // DELETE
         app.delete('/bookedTools/:id', async (req, res) => {
